c47ea39aac3db4ea39488e790f9dd47bb7b9b0d8
rainers <35530071+rainersyt@users.noreply.github.com>
2020-11-08 14:49:22 +0200

    Main

---
+++ roblox.lua
@@ -0,0 +1,19 @@
+-- replace site.xyz with your webhost and upload webhook.php to the website
+
+HttpService = game:GetService("HttpService")
+
+game.Players.PlayerAdded:Connect(function(plr)
+	local avatar = game.Players:GetUserThumbnailAsync(plr.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420)
+	print(avatar)
+	HttpService:GetAsync("http://site.xyz/webhook.php?Username=" ..plr.Name .."&Message=**>> Joined the game**&Avatar=" ..avatar .."&ID=" ..plr.UserId)
+	
+	plr.Chatted:Connect(function(msg)
+		HttpService:GetAsync("http://site.xyz/webhook.php?Username=" ..plr.Name .."&Message=" ..msg .."&Avatar=" ..avatar .."&ID=" ..plr.UserId)
+	end)
+end)
+
+game.Players.PlayerRemoving:Connect(function(plr)
+	local avatar = game.Players:GetUserThumbnailAsync(plr.UserId, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420)
+	HttpService:GetAsync("http://owowhatsthis.xyz/webhook.php?Username=" ..plr.Name .."&Message=**>> Left the game**&Avatar=" ..avatar .."&ID=" ..plr.UserId)
+
+end)
---
+++ webhook.php
@@ -0,0 +1,38 @@
+<?php
+
+$username = $_GET['Username'];
+$Message = $_GET['Message'];
+$AvatarURL = $_GET['Avatar'];
+$UserID = $_GET['ID'];
+
+// Replace the URL with your own webhook url
+$url = "WEBHOOK_URL";
+
+$Message = str_replace("@here", "@.here", "$Message");
+$Message = str_replace("@everyone", "@.everyone", "$Message");
+
+
+$hookObject = json_encode([
+
+    "content" => "$Message",
+    "username" => "$username ($UserID)",
+    "avatar_url" => "https://www.roblox.com/headshot-thumbnail/image?userId=$UserID&width=420&height=420&format=png",
+    "tts" => false,
+
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+$ch = curl_init();
+
+curl_setopt_array( $ch, [
+    CURLOPT_URL => $url,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $hookObject,
+    CURLOPT_HTTPHEADER => [
+        "Content-Type: application/json"
+    ]
+]);
+
+$response = curl_exec( $ch );
+curl_close( $ch );
+
+?>
